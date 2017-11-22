cc130f28d77dadea19ac1eeba2f1b5ccc6552097
Matt Tuchfarber <mtuchfarber@edx.org>
2017-11-22 14:20:02 -0500

    Basic bio showing

--- lms/static/js/program_marketing.js
+++ lms/static/js/program_marketing.js
@@ -4,3 +4,8 @@ function playVideo(src) {
     document.querySelector('#program_video iframe').style = 'display:block;';
     document.querySelector('#program_video iframe').src = src;
 }
+function showBio(button, bio_hash){
+    button.style.display='none';
+    var bio_id = 'instructor-bio-' + bio_hash;
+    document.getElementById(bio_id).style.display='block';
+}
